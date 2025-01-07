d325246d2275d631a2092d25b539ced3cae2e9e5
Ama Nkansah <amankansahstate@gmail.com>
2025-01-07 17:59:05 +0000

    added conditionals to my function

--- Problem_Solving/rock_paper_scissors/index.js
+++ Problem_Solving/rock_paper_scissors/index.js
@@ -1,7 +1,15 @@
 function getComputerChoice () {
- let randomnumber = Math.random();
- return randomnumber;
-}
+ let randomnumber = Math.random()
+
+ if(randomnumber < 0.4){
+  return "rock";
+ } else if(randomnumber < 0.8){
+    return "paper";
+  } else {
+    return "scissors";
+   }
+ }
+
 
 const result = getComputerChoice();
 console.log(result);
