ac11330123408073310110a5f61deef72223aaf3
Ama Nkansah <amankansahstate@gmail.com>
2025-01-08 13:08:19 +0000

    done but having errors

--- Problem_Solving/rock_paper_scissors/index.js
+++ Problem_Solving/rock_paper_scissors/index.js
@@ -1,12 +1,19 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function 
-
 
 function getHumanChoice () {
-  prompt("Enter a number between 0 and 1");
-  
+  let myPrompt= prompt("Enter a number between 0 and 1");
+
+  if(myPrompt < 0.4){
+    return "rock";
+   } else if( myPrompt < 0.8){
+      return "paper";
+    } else {
+      return "scissors";
+     }
+}
+
 function getComputerChoice () {
  let randomnumber = Math.random()
 
@@ -18,10 +25,29 @@ function getComputerChoice () {
     return "scissors";
    }
  }
- const result = getComputerChoice();
-alert(result);
+ humanScore = getHumanChoice ();
+ computerScore = getComputerChoice ();
+
+function playGame (){
+  function playRound (humanChoice,computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanScore > computerChoice){
+    console.log( `${humanScore} is winner over ${computerScore}` );
+    humanScore++;
+  } else if (computerChoice > humanScore ) {
+    console.log( `${computerScore} is winner over ${humanScore}` );
+    computerChoice++;
+  }else{ console.log( `It's a draw` );  
+  }
+  }
+  const gameResult = playGame ();
+  alert( gameResult);
 }
 
-console.log(getHumanChoice ());
+playGame ();
+
+
 
 
